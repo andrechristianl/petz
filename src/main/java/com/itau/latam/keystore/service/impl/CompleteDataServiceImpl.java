9fc693474667d73f18419ed4e57565dead5e54c5
Igor Limeira <igor.limeira@outlook.com>
2019-11-08 17:25:18 -0300

    delete service of encrypting and decrypting and model service impl to receive external service

--- src/main/java/com/itau/latam/keystore/service/impl/CompleteDataServiceImpl.java
+++ src/main/java/com/itau/latam/keystore/service/impl/CompleteDataServiceImpl.java
@@ -19,27 +19,16 @@ import com.itau.latam.keystore.service.CompleteDataService;
 @Service
 public class CompleteDataServiceImpl implements CompleteDataService{
 	
-	KeyGenerator keyGenerator = null;
-	
-    SecretKey key = null;
-
-    // Generating IV.
-    byte[] IV = new byte[16];
-    private SecureRandom random = new SecureRandom();
-    
+	 
 	@Override
 	public List<CompleteDataDTO> encryptData(List<CompleteDataDTO> completeDataDTO) {
 		try
-	    {    
-			 keyGenerator = KeyGenerator.getInstance("AES");
-			 keyGenerator.init(256);
-			 key = keyGenerator.generateKey();
-			 random.nextBytes(IV);
-				
+	    {    	
 			 completeDataDTO.stream().forEach(c -> 
 		          {
 					try {
-						c.setEncryptted(Base64.getEncoder().encodeToString(encrypt(c.getPlaintext().getBytes(),key,IV )));
+						//Inserir chamada do serviço encriptador
+						c.setEncryptted(c.getPlaintext());
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
@@ -63,7 +52,8 @@ public class CompleteDataServiceImpl implements CompleteDataService{
 			completeDataDTO.stream().forEach(c -> 
 	          {
 				try {
-					c.setPlaintext(decrypt(Base64.getDecoder().decode(c.getEncryptted()),key, IV));
+					//Inserir chamada do serviço decryptador
+					c.setPlaintext(c.getEncryptted());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -79,44 +69,4 @@ public class CompleteDataServiceImpl implements CompleteDataService{
 	        return null;
 	    }
 	}
-	
-	public static byte[] encrypt (byte[] plaintext,SecretKey key,byte[] IV ) throws Exception
-    {
-        //Get Cipher Instance
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        
-        //Create SecretKeySpec
-        SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
-        
-        //Create IvParameterSpec
-        IvParameterSpec ivSpec = new IvParameterSpec(IV);
-        
-        //Initialize Cipher for ENCRYPT_MODE
-        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
-        
-        //Perform Encryption
-        byte[] cipherText = cipher.doFinal(plaintext);
-        
-        return cipherText;
-    }
-    
-    public static String decrypt (byte[] cipherText, SecretKey key,byte[] IV) throws Exception
-    {
-        //Get Cipher Instance
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        
-        //Create SecretKeySpec
-        SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
-        
-        //Create IvParameterSpec
-        IvParameterSpec ivSpec = new IvParameterSpec(IV);
-        
-        //Initialize Cipher for DECRYPT_MODE
-        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
-        
-        //Perform Decryption
-        byte[] decryptedText = cipher.doFinal(cipherText);
-        
-        return new String(decryptedText);
-    }
 }
