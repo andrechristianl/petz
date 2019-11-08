c777b748a8004425595ae2b3c118e49e2a5b78fd
Felipe Bellettato (Jarvis) <fgbelet@itau-unibanco.com>
2019-11-08 19:38:21 -0200

    adding caller for encrypt/decrypt methods

--- src/main/java/com/itau/latam/keystore/service/impl/CompleteDataServiceImpl.java
+++ src/main/java/com/itau/latam/keystore/service/impl/CompleteDataServiceImpl.java
@@ -2,71 +2,54 @@ package com.itau.latam.keystore.service.impl;
 
 import java.util.List;
 
-import java.security.SecureRandom;
-import java.util.Base64;
-
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-
 import org.springframework.stereotype.Service;
 
 import com.itau.latam.keystore.model.dto.CompleteDataDTO;
 import com.itau.latam.keystore.service.CompleteDataService;
+import com.itau.latam.keystore.util.AESCBC256;
 
 @Service
-public class CompleteDataServiceImpl implements CompleteDataService{
-	
-	 
-	@Override
-	public List<CompleteDataDTO> encryptData(List<CompleteDataDTO> completeDataDTO) {
-		try
-	    {    	
-			 completeDataDTO.stream().forEach(c -> 
-		          {
-					try {
-						//Inserir chamada do serviço encriptador
-						c.setEncryptted(c.getPlaintext());
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			
-		     );
-	        return completeDataDTO;
-	    } 
-	    catch (Exception e) 
-	    {
-	        System.out.println("Error while encrypting: " + e.toString());
-	        return null;
-	    }
-	    
-	}
+public class CompleteDataServiceImpl implements CompleteDataService {
 
-	@Override
-	public List<CompleteDataDTO> decryptData(List<CompleteDataDTO> completeDataDTO) {
-		try
-	    {
-			completeDataDTO.stream().forEach(c -> 
-	          {
-				try {
-					//Inserir chamada do serviço decryptador
-					c.setPlaintext(c.getEncryptted());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		
-	     );
-      return completeDataDTO;
+    @Override
+    public List<CompleteDataDTO> encryptData(List<CompleteDataDTO> completeDataDTO) {
+        try {
+            completeDataDTO.stream().forEach(c -> {
+                try {
+                    // Inserir chamada do serviço encriptador
+                    c.setEncryptted(AESCBC256.encrypt(c.getPlaintext()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
-	    } 
-	    catch (Exception e) 
-	    {
-	        System.out.println("Error while encrypting: " + e.toString());
-	        return null;
-	    }
-	}
+            );
+            return completeDataDTO;
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e.toString());
+            return null;
+        }
+
+    }
+
+    @Override
+    public List<CompleteDataDTO> decryptData(List<CompleteDataDTO> completeDataDTO) {
+        try {
+            completeDataDTO.stream().forEach(c -> {
+                try {
+                    // Inserir chamada do serviço decryptador
+                    c.setPlaintext(AESCBC256.decrypt(c.getEncryptted()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            );
+            return completeDataDTO;
+
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e.toString());
+            return null;
+        }
+    }
 }
