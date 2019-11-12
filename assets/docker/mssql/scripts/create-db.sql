0a53d65978e6decbde1dd464ebe332758ae8efb5
Felipe Bellettato (Jarvis) <fgbelet@itau-unibanco.com>
2019-11-12 21:54:32 -0200

    lowering key and salt sizes as per generated encrypted text's size constraint

--- assets/docker/mssql/scripts/create-db.sql
+++ assets/docker/mssql/scripts/create-db.sql
@@ -6,8 +6,8 @@ GO
 
 CREATE TABLE Stores.dbo.KeyStore (
 	id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	secret_key varchar(35) NOT NULL UNIQUE,
-	salt varchar(35) NOT NULL UNIQUE,
+	secret_key varchar(32) NOT NULL UNIQUE,
+	salt varchar(60) NOT NULL UNIQUE,
 	created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP UNIQUE
 );
 GO
