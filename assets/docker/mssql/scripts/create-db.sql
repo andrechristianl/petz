baf496dd1f5219563cc6cfb404838ab1487f559c
andrechristianl <andrechristianl@gmail.com>
2019-11-14 15:40:37 -0300

    change docker-compose

--- assets/docker/mssql/scripts/create-db.sql
+++ assets/docker/mssql/scripts/create-db.sql
@@ -6,8 +6,19 @@ GO
 
 CREATE TABLE Stores.dbo.KeyStore (
 	id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	secret_key varchar(32) NOT NULL UNIQUE,
-	salt varchar(60) NOT NULL UNIQUE,
+	secret_key varchar(35) NOT NULL UNIQUE,
+	salt varchar(35) NOT NULL UNIQUE,
 	created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP UNIQUE
+);
+GO
+
+
+INSERT INTO Stores.dbo.KeyStore (
+secret_key,
+salt
+)
+VALUES (
+'mssql-keystore-container',
+'mssql-keystore-container'
 );
 GO
