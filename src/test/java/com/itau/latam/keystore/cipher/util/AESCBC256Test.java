bf11cfc92c70277f0522b7f78763557e07657604
Felipe Bellettato (Jarvis) <fgbelet@itau-unibanco.com>
2019-11-18 17:28:48 -0200

    adding tests for the encryption and decryption utilitaries

--- src/test/java/com/itau/latam/keystore/cipher/util/AESCBC256Test.java
+++ src/test/java/com/itau/latam/keystore/cipher/util/AESCBC256Test.java
@@ -1,12 +1,16 @@
 package com.itau.latam.keystore.cipher.util;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.NoSuchPaddingException;
 
+import org.hibernate.service.spi.ServiceException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -25,37 +29,107 @@ public class AESCBC256Test {
         assertNotNull(ciphers.get(id));
     }
     
-    @Test(expected = RuntimeException.class)
-    public void validadeExceptionInTheCipherSuiteCreationWhenSaltIsNull() {
-        String secretKey = "foo";
-        String salt = null;
-        String id = "1";
-        AESCBC256.validateCipherSuite(secretKey, salt, id);
-    }
     
     @Test(expected = RuntimeException.class)
-    public void validadeExceptionInTheCipherSuiteCreationWhenKeyIsNull() {
-        String secretKey = null;
-        String salt = "bar";
-        String id = "1";
-        AESCBC256.validateCipherSuite(secretKey, salt, id);
-    }
-    
-    
-    
-    
-    
-    
-    @Test(expected = RuntimeException.class)
-    public void expetingExceptionWhileTryingToDecryptButTheCipherWasntProperlyInitialized() {
-        String encryptedData = "TWc9PS5PQ3hHQS9semQ2VFBsZDljUFI4cWdDNGR5UTRPTk5xNzRpOGYzTlpUK0s5NXc1bTBKazRaRkhudm5mK0RuSUI3Z3BXaHYzcnZMRXVJSzViMVd3MUNCUT09";
+    public void expectingExceptionWhileTryingToDecryptButTheCipherWasntProperlyInitialized() {
+        String encryptedData = "TVE9PS4zaFBCcEhvNEFIREQzMzBGMkM3YTZ2VkFmd1lNUU1zZkVlZ2Via2loVVZ4YWlacVkzcTlEL1BmZmRGQkpTcDVtWWFBaS9ZQ3ZrQkpsVHF2anhOdm53QT09";
         AESCBC256.generateFinalDecryptedData(encryptedData);
     }
     
-    // TODO: Mock the static cipher somehow
-//    @Test
-//    public void someTest() {
-//        String encryptedData = "TWc9PS5PQ3hHQS9semQ2VFBsZDljUFI4cWdDNGR5UTRPTk5xNzRpOGYzTlpUK0s5NXc1bTBKazRaRkhudm5mK0RuSUI3Z3BXaHYzcnZMRXVJSzViMVd3MUNCUT09";
-//        AESCBC256.generateFinalDecryptedData(encryptedData);
-//    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    //  encrypted-text -> TVE9PS5TcVdpUXYwcVVueFdacHh0SHNGM3B3PT0=
+    //
+    //  Solves to/for: 
+    //      key -> foo , 
+    //      salt" -> bar, 
+    //      clear-text -> hello
+    /////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void validateCorrectDecryption() throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
+        String encryptedData = "TVE9PS5TcVdpUXYwcVVueFdacHh0SHNGM3B3PT0=";
+
+        HashMap<String, CipherSuite> suite = new HashMap<String, CipherSuite>();
+        suite.put("1", new CipherSuite("foo", "bar"));
+        AESCBC256.setCiphers(suite);
+        
+        String result = AESCBC256.generateFinalDecryptedData(encryptedData);
+        assertEquals(result, "hello");
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void expectingExceptionWhenIdToSearchDIffersFromIdInsideEncryptedData() throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
+        String encryptedData = "TVE9PS5TcVdpUXYwcVVueFdacHh0SHNGM3B3PT0=";
+
+        HashMap<String, CipherSuite> suite = new HashMap<String, CipherSuite>();
+        suite.put("2", new CipherSuite("foo", "bar"));
+        AESCBC256.setCiphers(suite);
+        
+        AESCBC256.generateFinalDecryptedData(encryptedData);
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void expectingExceptionWhenSaltIsIncorret() throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
+        String encryptedData = "TVE9PS5TcVdpUXYwcVVueFdacHh0SHNGM3B3PT0=";
+
+        HashMap<String, CipherSuite> suite = new HashMap<String, CipherSuite>();
+        suite.put("1", new CipherSuite("jarvis", "bar"));
+        AESCBC256.setCiphers(suite);
+        
+        AESCBC256.generateFinalDecryptedData(encryptedData);
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void expectingExceptionWhenKeyIsIncorret() throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
+        String encryptedData = "TVE9PS5TcVdpUXYwcVVueFdacHh0SHNGM3B3PT0=";
+
+        HashMap<String, CipherSuite> suite = new HashMap<String, CipherSuite>();
+        suite.put("1", new CipherSuite("foo", "jarvis"));
+        AESCBC256.setCiphers(suite);
+        
+        AESCBC256.generateFinalDecryptedData(encryptedData);
+    }
+    
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    //  encrypted-text -> TVE9PS4zaFBCcEhvNEFIREQzMzBGMkM3YTZ2VkFmd1lNUU1zZkVlZ2Via2loVVZ4YWlacVkzcTlEL1BmZmRGQkpTcDVtZlR3Z1pEeXFjZXVSR2xiSHI1b3FJZz09
+    //
+    //  Solves to/for: 
+    //      key -> foo , 
+    //      salt" -> bar, 
+    //      clear-text -> 1234567890123456789012345678901234567890123456789012345
+    /////////////////////////////////////////////////////////////////////////////////////////    
+    @Test()
+    public void validateCorrectEncryption() throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
+        String clearText = "1234567890123456789012345678901234567890123456789012345";
+
+        HashMap<String, CipherSuite> suite = new HashMap<String, CipherSuite>();
+        suite.put("1", new CipherSuite("foo", "bar"));
+        AESCBC256.setCiphers(suite);
+        AESCBC256.setLastId("1");
+        
+        String result = AESCBC256.generateFinalEncryptedData(clearText);
+        
+        assertEquals(result, "TVE9PS4zaFBCcEhvNEFIREQzMzBGMkM3YTZ2VkFmd1lNUU1zZkVlZ2Via2loVVZ4YWlacVkzcTlEL1BmZmRGQkpTcDVtZlR3Z1pEeXFjZXVSR2xiSHI1b3FJZz09");
+    }
+    
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    //  encrypted-text -> TVE9PS4zaFBCcEhvNEFIREQzMzBGMkM3YTZ2VkFmd1lNUU1zZkVlZ2Via2loVVZ4YWlacVkzcTlEL1BmZmRGQkpTcDVtUFRMQ0lDSHhVMmZERHp0cjVaeU9MQT09
+    //
+    //  Solves to/for: 
+    //      key -> foo , 
+    //      salt" -> bar, 
+    //      clear-text -> 12345678901234567890123456789012345678901234567890123456
+    /////////////////////////////////////////////////////////////////////////////////////////
+    @Test(expected = ServiceException.class)
+    public void expectingExceptionWhenClearTextHas56OrMoreCharacters() throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
+        String clearText = "12345678901234567890123456789012345678901234567890123456";
+
+        HashMap<String, CipherSuite> suite = new HashMap<String, CipherSuite>();
+        suite.put("1", new CipherSuite("foo", "bar"));
+        AESCBC256.setCiphers(suite);
+        
+        AESCBC256.generateFinalEncryptedData(clearText);
+    }
 }
